9f2ff7ae3c45dff8130836da03b38744c31c994a
dustinlarimer <dustinlarimer@gmail.com>
2016-03-10 18:09:57 -0800

    Debug, fix some things, add masterKey()

--- test/index.js
+++ test/index.js
@@ -7,3 +7,27 @@ test('Constructor', function(t) {
     'Instantiation should return a new instance of the app');
   t.end();
 });
+
+test('.masterKey()', function(t){
+  var app = new App({
+    masterKey: '123'
+  });
+  t.equal(app.masterKey(), '123',
+    'Should get the correct masterKey value');
+  app.masterKey('456');
+  t.equal(app.masterKey(), '456',
+    'Should set the correct masterKey value');
+  t.end();
+});
+
+test('.projectId()', function(t){
+  var app = new App({
+    projectId: '123'
+  });
+  t.equal(app.projectId(), '123',
+    'Should get the correct projectId value');
+  app.projectId('456');
+  t.equal(app.projectId(), '456',
+    'Should set the correct projectId value');
+  t.end();
+});
